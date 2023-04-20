31e3e12929c837a407c538a380b7fb39c8686852
Geraldy Kumara <76173300+GeraldyK@users.noreply.github.com>
2023-04-20 16:01:33 +0700

    Initial commit

---
+++ JungwookTeam/JungwookTeam/JungwookTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  JungwookTeamApp.swift
+//  JungwookTeam
+//
+//  Created by Geraldy Kumara on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct JungwookTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
