66f060d5d83dfea13eb4de9a5ae0bef5c8b14108
GeraldyK <76173300+GeraldyK@users.noreply.github.com>
2023-04-28 16:43:06 +0700

    tes commit

---
+++ JungwookTeam/JungwookTeam/DropdownMenu/DropdownList.swift
@@ -0,0 +1,25 @@
+////
+////  DropdownList.swift
+////  JungwookTeam
+////
+////  Created by Geraldy Kumara on 27/04/23.
+////
+//
+//import SwiftUI
+//
+//struct DropdownList: View {
+//    let option: [DropdownTest]
+//    var body: some View {
+//        LazyVStack (alignment: .leading, spacing: 2){
+//            ForEach(option) { option in
+//                
+//            }
+//        }
+//    }
+//}
+//
+//struct DropdownList_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DropdownList(option: DropdownTest.testAllPlayer)
+//    }
+//}
--- JungwookTeam/JungwookTeam/DropdownMenu/DropdownTestView.swift
+++ JungwookTeam/JungwookTeam/DropdownMenu/DropdownTestView.swift
@@ -1,44 +1,65 @@
+////
+////  DropdownTestView.swift
+////  JungwookTeam
+////
+////  Created by Geraldy Kumara on 27/04/23.
+////
 //
-//  DropdownTestView.swift
-//  JungwookTeam
+//import SwiftUI
 //
-//  Created by Geraldy Kumara on 27/04/23.
+//struct DropdownTestView: View {
+//    
+//    //untuk ngeliat apakah user melakukan opsi selection atau tidak
+//    @State private var isOption: Bool = false
+//    
+//    // untuk ngebind pilihan dari user
+//    @Binding var selectedOption: DropdownTest?
+//    
+//    // Untuk placeholder dropdown menu
+//    let placeholder: String
+//    
+//    //pilihan" option
+//    let option: [DropdownTest]
+//    
+//    var body: some View {
+//        Button(action: {
+//            withAnimation{
+//                self.isOption.toggle()
+//            }
+//        }) {
+//            HStack{
+//                Text(selectedOption == nil ? placeholder : selectedOption!.option)
+//                    .fontWeight(.medium)
+//                    .foregroundColor(.black)
+//                
+//                Spacer()
+//                
+//                Image(systemName: self.isOption ? "chevron.up" : "chevron.down")
+//                    .fontWeight(.medium)
+//                    .foregroundColor(.black)
+//            }
+//        }
+//        .padding()
+//        .overlay{
+//            RoundedRectangle(cornerRadius: 5)
+//                .stroke(.gray, lineWidth: 2)
+//        }
+//        .overlay{
+//            VStack{
+//                self.isOption{
+//                    Spacer(minLength: 60)
+//                }
+//            }
+//        }
+//        .padding(.horizontal)
+//    }
+//}
 //
-
-import SwiftUI
-
-struct DropdownTestView: View {
-    
-    //untuk ngeliat apakah user melakukan opsi selection atau tidak
-    @State private var isOption: Bool = false
-    
-    // untuk ngebind pilihan dari user
-    @Binding var selectedOption: DropdownTest?
-    
-    // Untuk placeholder dropdown menu
-    let placeholder: String
-    
-    //pilihan" option
-    let option: [DropdownTest]
-    
-    var body: some View {
-        Button(action: {
-            withAnimation{
-                self.isOption.toggle()
-            }
-        }) {
-            HStack{
-                Text(selectedOption == nil ? placeholder : selectedOption!.option)
-            }
-        }
-    }
-}
-
-struct DropdownTestView_Previews: PreviewProvider {
-    static var previews: some View {
-        DropdownTestView(
-            selectedOption: .constant(nil),
-            placeholder: "Select your month",
-            option: DropdownTest.testAllPlayer)
-    }
-}
+//struct DropdownTestView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DropdownTestView(
+//            selectedOption: .constant(nil),
+//            placeholder: "Select your month",
+//            option: DropdownTest.testAllPlayer)
+//    }
+//}
