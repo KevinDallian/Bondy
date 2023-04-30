1b9d1c384bbb833530da797ce81265340bfa4b96
GeraldyK <76173300+GeraldyK@users.noreply.github.com>
2023-04-30 19:24:28 +0700

    added and delete file

---
+++ JungwookTeam/JungwookTeam/Features/Model/OnBoardingModel.swift
@@ -0,0 +1,14 @@
+//
+//  OnBoardingModel.swift
+//  JungwookTeam
+//
+//  Created by Geraldy Kumara on 30/04/23.
+//
+
+import Foundation
+
+struct OnBoarding: Identifiable {
+    let id = UUID().uuidString
+    let image: String
+    let description: String
+}
